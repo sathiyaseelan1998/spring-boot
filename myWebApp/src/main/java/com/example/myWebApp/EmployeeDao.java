14afebef7ff30e6dcc89a6f9a75b7d1b8e926ffb
sathiyaseelan <sathiyaseelan2598@gmail.com>
2022-12-31 10:23:27 +0530

    add jars

--- myWebApp/src/main/java/com/example/myWebApp/EmployeeDao.java
+++ myWebApp/src/main/java/com/example/myWebApp/EmployeeDao.java
@@ -1,7 +1,8 @@
 package com.example.myWebApp;
 
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface EmployeeDao extends CrudRepository<EmployeeBo, Integer> {
 
 }
